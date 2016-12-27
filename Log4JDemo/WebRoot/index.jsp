cf0f81dc78d0fa523249ebd20135835ac6840d55
LonelyTear <love13579>
2016-12-27 14:37:19 +0800

    2

--- Log4JDemo/WebRoot/index.jsp
+++ Log4JDemo/WebRoot/index.jsp
@@ -21,6 +21,6 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
   </head>
   
   <body>
-    This is my JSP page. <br>
+    log4j. <br>
   </body>
 </html>
--- Log4JDemo/src/king/Log4JDemo.java
+++ Log4JDemo/src/king/Log4JDemo.java
@@ -9,8 +9,9 @@ public class Log4JDemo {
 	public static void main(String[] args) {
 		Logger logger = Logger.getLogger(Log4JDemo.class);
 		PropertyConfigurator.configure("src/log4j.properties");
+		// 万全之法
 		PropertyConfigurator.configure(ClassLoader.getSystemResource("log4j.properties"));
-//		logger.setLevel(Level.WARN ); //强制logger级别
+		// logger.setLevel(Level.WARN ); //强制logger级别
 		logger.error("error");
 		logger.warn("warn");
 		logger.info("info");
